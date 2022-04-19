aa796ec30510ccf8114e40dd800e66c7667d2ef3
faizan-141 <faizanfarooq141@gmail.com>
2022-04-20 00:03:48 +0530

    adding to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Display = () => {
+  return (
+    <div id='display'>
+      <div className="equation-diplay">0</div>
+      <div className="main-display">0</div>
+    </div>
+  )
+}
+
+const KeyPad = () => {
+  return (
+    <div id='key-pad'>
+      <div>AC</div>
+      <div>/</div>
+      <div>x</div>
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>=</div>
+      <div>0</div>
+      <div>.</div>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className="cal-body">
+        <Display />
+        <KeyPad />
+      </div>
     </div>
   );
 }
